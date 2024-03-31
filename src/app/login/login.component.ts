823cdbc3d5bd1b0fc814ce6c20168d8ffe92bae0
Fumas <vinifumagalli_@hotmail.com>
2024-03-31 17:11:18 -0300

    refactor (login): altera id do html e corrige function no ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { LoginUserViewModel } from '../api/loginUserViewModel';
 import { ApiService } from '../services/api.service';
 import { Resposta } from '../api/resposta';
 import { MessageService } from 'primeng/api';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -20,18 +21,32 @@ export class LoginComponent {
   
   async login() {
     
-    (await this.apiService.enviarDados('auth/sign-in', this.user)).subscribe((res: Resposta) => {
-      if(res.sucesso) {
+    // (await this.apiService.enviarDados('auth/sign-in', this.user))
+    // .subscribe((res: Resposta) => {
+    //   if(res.sucesso) {
+    //     this.msgService.add({ severity:'success', summary:'Sucesso', detail:res.mensagem});
+    //     sessionStorage.setItem('token', res.data.accessToken);
+    //     sessionStorage.setItem('usuario', res.data.userToken.email);
+    //   } 
+    // }, (fail: HttpErrorResponse) => {
+    //     this.msgService.add({ severity:'error', summary:'Erro', detail: fail.error.mensagem});
+    //   })
+
+    (await this.apiService.enviarDados('auth/sign-in', this.user))
+    .subscribe(
+      {
+        next: (res:Resposta) => {
         this.msgService.add({ severity:'success', summary:'Sucesso', detail:res.mensagem});
         sessionStorage.setItem('token', res.data.accessToken);
         sessionStorage.setItem('usuario', res.data.userToken.email);
-      } 
-    }, error => {
-      if(error.status === 400) {
-        this.msgService.add({ severity:'error', summary:'Erro', detail:'Usuário ou senha inválidos'});
+        sessionStorage.setItem('perfil', res.data.userToken.claims[0].type);
+        sessionStorage.setItem('permissoes', res.data.userToken.claims[0].value);
+        },
+        error: (fail:HttpErrorResponse) => {
+          this.msgService.add({ severity:'error', summary:'Erro', detail:fail.error.mensagem });
+        }
       }
-    })
-    
+    )
   }
   
 }
