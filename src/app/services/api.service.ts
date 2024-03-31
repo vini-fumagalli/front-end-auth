86b35e38762eaa31b16d8c52e438c7022dfe4b94
Fumas <vinifumagalli_@hotmail.com>
2024-03-31 18:13:50 -0300

    feat (apiService): implementa header com token nas requests

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { API_URL } from '../enviroment/enviroment';
 import { Resposta } from '../api/resposta';
@@ -11,8 +11,21 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
+  getHeader(): HttpHeaders {
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+    });
+  }
+
   // Exemplo de método para obter dados da API
   async enviarDados(endpoint:string, parameter:any): Promise<Observable<Resposta>> {
-    return this.http.post<Resposta>(`${API_URL}/${endpoint}`, parameter);
+    return this.http
+    .post<Resposta>(`${API_URL}/${endpoint}`, parameter, { headers:this.getHeader() });
+  }
+
+  async receberDados(endpoint:string): Promise<Observable<Resposta>> {
+    return this.http
+    .get<Resposta>(`${API_URL}/${endpoint}`, { headers: this.getHeader() });
   }
 }
