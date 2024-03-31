5fcd0fe274a32d7a3253666894e7cb31e09ec479
Fumas <vinifumagalli_@hotmail.com>
2024-03-31 17:12:08 -0300

    feat (cadastro-usuario): adiciona tratatiava de erro em resposta do back-end

--- src/app/cadastro-usuario/cadastro-usuario.component.ts
+++ src/app/cadastro-usuario/cadastro-usuario.component.ts
@@ -3,6 +3,7 @@ import { RegisterUserViewModel } from '../api/registerUserViewModel';
 import { ApiService } from '../services/api.service';
 import { Resposta } from '../api/resposta';
 import { MessageService } from 'primeng/api';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-cadastro-usuario',
@@ -24,12 +25,18 @@ export class CadastroUsuarioComponent {
       return this.msgService.add({ severity:'error', summary:'Erro', detail:'Senhas não conferem' });
     }
 
-    (await this.apiService.enviarDados('auth/sign-up', this.newUser)).subscribe((res:Resposta) => {
-      if(res.sucesso) {
-        this.msgService.add({ severity:'success', summary:'Sucesso', detail:res.mensagem });
-        sessionStorage.setItem('token', res.data.accessToken);
-        sessionStorage.setItem('usuario', res.data.userToken.email);
+    (await this.apiService.enviarDados('auth/sign-up', this.newUser))
+    .subscribe(
+      {
+        next: (res:Resposta) => {
+          this.msgService.add({ severity:'success', summary:'Sucesso', detail:res.mensagem });
+          sessionStorage.setItem('token', res.data.accessToken);
+          sessionStorage.setItem('usuario', res.data.userToken.email);
+        },
+        error: (fail:HttpErrorResponse) => {
+          this.msgService.add({ severity: 'error', summary: 'Erro', detail: fail.error.mensagem });
+        }
       }
-    });
+    )
   }
-}
+} 
