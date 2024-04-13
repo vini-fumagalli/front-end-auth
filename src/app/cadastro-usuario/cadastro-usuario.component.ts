7fae7dafa739312f53c00aa0bd8ec3b80fe61af1
Fumas <vinifumagalli_@hotmail.com>
2024-04-13 18:43:52 -0300

    feat (cadastro-usuario): implementa form de código de autenticação

--- src/app/cadastro-usuario/cadastro-usuario.component.ts
+++ src/app/cadastro-usuario/cadastro-usuario.component.ts
@@ -4,6 +4,8 @@ import { ApiService } from '../services/api.service';
 import { Resposta } from '../api/resposta';
 import { MessageService } from 'primeng/api';
 import { HttpErrorResponse } from '@angular/common/http';
+import { CodAutViewModel } from '../api/codAutViewModel';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro-usuario',
@@ -12,11 +14,16 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 
 export class CadastroUsuarioComponent {
+
   newUser = new RegisterUserViewModel();
+  codAutVm = new CodAutViewModel();
+  showFormCodAut:boolean = false;
+  showFormSignUp:boolean = true;
 
   constructor(
       private apiService:ApiService, 
-      private msgService: MessageService
+      private msgService: MessageService,
+      private router:Router
       ) { }
 
   async register() {
@@ -25,18 +32,56 @@ export class CadastroUsuarioComponent {
       return this.msgService.add({ severity:'error', summary:'Erro', detail:'Senhas não conferem' });
     }
 
+    if(!this.senhaValida(this.newUser.password)) {
+      return this.msgService.add({ severity:'error', summary:'Erro', detail:'Senha precisa ter pelo menos 6 caracteres com ao menos 1 letra e 1 número' });
+    }
+
     (await this.apiService.enviarDados('auth/sign-up', this.newUser))
     .subscribe(
       {
         next: (res:Resposta) => {
           this.msgService.add({ severity:'success', summary:'Sucesso', detail:res.mensagem });
-          sessionStorage.setItem('token', res.data.accessToken);
-          sessionStorage.setItem('usuario', res.data.userToken.email);
+          this.showFormSignUp = false;
+          this.showFormCodAut = true;
         },
         error: (fail:HttpErrorResponse) => {
-          this.msgService.add({ severity: 'error', summary: 'Erro', detail: fail.error.mensagem });
+          if(fail.status === 409) {
+            return this.msgService.add({ severity: 'error', summary: 'Erro', detail: fail.error });
+          }
+          this.msgService.add({ severity: 'error', summary: 'Erro', detail: 'Ocorreu um erro ao enviar o email. Por favor tente novamente' });
         }
       }
     )
+  }
+
+  async codAut() {
+    if(this.codAutVm.codAut.length !== 6) {
+      this.msgService.add({ severity: 'error', summary: 'Erro', detail:'Código inválido'})
+    }
+
+    this.codAutVm.email = this.newUser.email;
+    this.codAutVm.password = this.newUser.password;
+
+    (await this.apiService.enviarDados('auth/cod-aut', this.codAutVm))
+    .subscribe(
+      {
+        next: (res:Resposta) => {
+          this.msgService.add({severity:'success', summary: 'Sucesso', detail:res.mensagem});
+          sessionStorage.setItem('token', res.data.accessToken);
+          sessionStorage.setItem('expiresAt', res.data.expiresAt);
+          sessionStorage.setItem('usuario', res.data.userToken.email);
+          this.router.navigate(['/home'])
+        },
+        error: (fail:HttpErrorResponse) => {
+          this.msgService.add({ severity: 'error', summary: 'Erro', detail: fail.error });
+        }
+      }
+    )
+
+  }
+
+  senhaValida(password:string) {
+    const regex = /^(?=.*[a-z])(?=.*\d).{6,}$/;
+    return regex.test(password);
   }
 } 
