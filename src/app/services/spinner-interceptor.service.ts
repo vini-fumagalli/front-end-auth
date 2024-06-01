e8e5a9afeb73826036f14cd4d3ed2fea15d5d963
Fumas <vinifumagalli_@hotmail.com>
2024-06-01 12:51:03 -0300

    feat (spinner-interceptor): adiciona um handle error

--- src/app/services/spinner-interceptor.service.ts
+++ src/app/services/spinner-interceptor.service.ts
@@ -3,12 +3,15 @@ import {
   HttpInterceptor,
   HttpRequest,
   HttpHandler,
-  HttpEvent
+  HttpEvent,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { finalize } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { catchError, finalize } from 'rxjs/operators';
 import { SpinnerService } from './spinner.service';
 import { AuthService } from '../security/auth.service';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 
 
 @Injectable({
@@ -17,7 +20,9 @@ import { AuthService } from '../security/auth.service';
 export class HttpInterceptorService implements HttpInterceptor {
   constructor(
     private spinnerService: SpinnerService,
-    private authService:AuthService
+    private authService:AuthService,
+    private router:Router,
+    private msgService:MessageService
     ) { }
 
 
@@ -33,9 +38,34 @@ export class HttpInterceptorService implements HttpInterceptor {
       },
     });
     return next.handle(tokenReq).pipe(
+      // catchError((err: HttpErrorResponse) => {
+      //   return this.handlerError(err);
+      // }),
       finalize(() => {
         this.spinnerService.hide();
       })
     );
   }
+
+  // handlerError(err: HttpErrorResponse) {
+  //   if(err.status === 401) {
+  //     this.router.navigate(['/login']);
+  //     sessionStorage.clear();
+  //     this.msgService.add({ severity:'warn', summary:'Aviso', detail:'Sua sessão expirou. Faça login novamente'});
+  //   }
+
+  //   if(err.status === 403) {
+  //     this.msgService.add({ severity:'error', summary:'Aviso', detail:'Você não tem permissão para isso'});
+  //   }
+
+  //   if(err.status === 500) {
+  //     this.msgService.add({ severity:'error', summary:'Erro', detail:'Ocorrerão erros. Tente novamente ou contate o suporte'});
+  //   }
+
+  //   if(err.status === 400) {
+  //     this.msgService.add({ severity:'error', summary:'Erro', detail:'Informações mal inseridas'});
+  //   }
+
+  //   return throwError(() => new Error('Ocorreu um erro'));
+  // }
 }
